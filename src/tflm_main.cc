98663972622bbbd0b48dbf93f2021878e1d065b9
Yen-Chieh Huang <lashhw81@gmail.com>
2026-09-08 14:47:36 +0800

    static allocation

--- src/tflm_main.cc
+++ src/tflm_main.cc
@@ -1,3 +1,5 @@
+#include <new>
+
 #include "tflm_main.h"
 
 #include "tensorflow/lite/micro/micro_interpreter.h"
@@ -5,6 +7,7 @@
 #include "tensorflow/lite/micro/system_setup.h"
 
 namespace {
+alignas(tflite::MicroInterpreter) unsigned char g_interpreter_storage[sizeof(tflite::MicroInterpreter)];
 tflite::MicroInterpreter* g_interpreter = nullptr;
 TflmTensor g_input = {};
 TflmTensor g_output = {};
@@ -48,13 +51,14 @@ TflmStatus InitializeModel(const unsigned char* model_data, AddOpsFn add_ops,
   TFLM_ENSURE(resolver_status == kTfLiteOk);
 
   if (g_interpreter) {
-    delete g_interpreter;
+    g_interpreter->~MicroInterpreter();
     g_interpreter = nullptr;
   }
   g_input = {};
   g_output = {};
 
-  g_interpreter = new tflite::MicroInterpreter(model, resolver, tensor_arena, tensor_arena_size);
+  g_interpreter = new (g_interpreter_storage) tflite::MicroInterpreter(model, resolver, tensor_arena, tensor_arena_size);
+  TFLM_ENSURE(g_interpreter->initialization_status() == kTfLiteOk);
   TFLM_ENSURE(g_interpreter->inputs_size() == 1 && g_interpreter->outputs_size() == 1);
   TFLM_ENSURE(g_interpreter->AllocateTensors() == kTfLiteOk);
 
